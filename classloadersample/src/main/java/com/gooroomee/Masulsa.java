14dbf97e4a700dea84b6f3b6ab41232629f25c94
_HanGyulKang <gang6607@gooroomee.com>
2024-07-15 16:08:55 +0900

    ButeBuddy로 바이트코드를 조작하는 방식을 jar로 패키징한 프로젝트를 사용해서 비침투적으로 처리 함(.class파일을 직접 조작하지 않고 메모리에서 가져올 때만 처리)

--- classloadersample/src/main/java/com/gooroomee/Masulsa.java
+++ classloadersample/src/main/java/com/gooroomee/Masulsa.java
@@ -1,13 +1,5 @@
 package com.gooroomee;
 
-import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.implementation.FixedValue;
-
-import java.io.File;
-import java.io.IOException;
-
-import static net.bytebuddy.matcher.ElementMatchers.named;
-
 public class Masulsa {
 
     // 바이트코드 조작 라이브러리
@@ -15,17 +7,11 @@ public class Masulsa {
     // 2. Javassist: https://www.javassist.org/
     // 3. ByteBuddy : https://bytebuddy.net/#/
     // ::: ByteBuddy 추천
-    public static void main(String... args) {
-        try {
-            new ByteBuddy().redefine(Moja.class)
-                           .method(named("pullOut"))
-                           .intercept(FixedValue.value("Rabbit!"))
-                           .make()
-                           .saveIn(new File("/Users/gooroomee/inflearn/project/classloadersample/target/classes/"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
+    // Masulsa 클래스 실행할 때 MasulsaAgent를 말은 jar 경로를 VM option에 넣어서 실행
+    // -javaagent:/Users/gooroomee/inflearn/project/MasulsaAgent/target/MasulsaAgent-1.0-SNAPSHOT.jar
+    // 기존 class 코드를 바꾸지 않고 처리 함 (=byte코드에 직접 변화를 주지 않고 메모리에서 가져올 때만 작동하게 함 : 비침투적임)
+    public static void main(String... args) {
         System.out.println(new Moja().pullOut());
     }
 }
